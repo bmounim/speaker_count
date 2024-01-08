0d41594e00f848075b893cb38ef4cef78fec06c1
belkhoumali-Ahmed-Abdelmounaim <ahmed_abd_el_mounaim.belkhoumali@g.enp.edu.dz>
2024-01-08 15:26:02 +0100

    Initial commit

---
+++ script.bat
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Navigate to the desired directory
+
+# Initialize a new Git repository
+git init
+
+# Add all files to the repository
+git add .
+
+# Commit the changes
+git commit -m "Initial commit"
+
+# Create a GitHub repository using GitHub CLIchmod +x script.sh
+# Push to the GitHub repository
+git push -u origin master
